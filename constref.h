ff263d69cfde8aaa851b46aa10590d21668c303a
Ondrej Novak <ondra-novak@email.cz>
2017-11-03 15:16:35 +0100

    added constref type

---
+++ constref.h
@@ -0,0 +1,36 @@
+#ifndef __ONDRA_SHARED_CONSTREF_H_98231adg28e7hbdiw6_
+#define __ONDRA_SHARED_CONSTREF_H_98231adg28e7hbdiw6_
+
+
+namespace ondra_shared {
+
+namespace _details {
+
+template<typename T>
+struct ConstRef {
+public:
+	typedef const T &Value;
+};
+
+template<typename T>
+struct ConstRef<T &> {
+public:
+	typedef T &Value;
+};
+
+template<typename T>
+struct ConstRef<const T &> {
+public:
+	typedef const T &Value;
+};
+
+
+}
+
+template<typename T>
+using const_ref = typename _details::ConstRef<T>::Value;
+
+
+}
+
+#endif
