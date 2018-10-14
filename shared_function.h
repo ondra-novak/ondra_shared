8f1c593c407feba0dce3c50ba38b89eae011b96f
Ondrej Novak <ondra-novak@email.cz>
2018-10-14 22:03:10 +0200

    improve aand test shared function

--- shared_function.h
+++ shared_function.h
@@ -8,21 +8,102 @@
 #ifndef ONDRA_SHARED_H_SHARED_FUNCTION_H_23232idewdkwp3dksmc
 #define ONDRA_SHARED_H_SHARED_FUNCTION_H_23232idewdkwp3dksmc
 
-#include "refcnt.h"
 #include <memory>
 #include <type_traits>
 #include <functional>
+#include <tuple>
+#include "refcnt.h"
+#include "apply.h"
 
 namespace ondra_shared {
+
+
 
 
 namespace _details {
 	template<typename T> struct ForceReference;
 	template<typename T> using ForceReference_t = typename ForceReference<T>::type;
+
+
+	template<typename Base>
+	class lambda_state_base_t: public Base {
+	public:
+		template <typename ... Types, std::size_t ... Indices>
+		lambda_state_base_t(const std::tuple<Types...> &tuple, std::index_sequence<Indices...>)
+			:Base(std::get<Indices>(tuple)...) {}
+
+
+	};
+
+
+
+	template<typename Base, typename ... Args>
+	class lambda_state_t: public lambda_state_base_t<Base> {
+	public:
+		lambda_state_t(_details::ForceReference_t<Args> ... args)
+			:lambda_state_base_t<Base>(std::tuple<Args...>(std::forward<Args>(args)...),std::index_sequence_for<Args...>())
+			,params(std::forward<_details::ForceReference_t<Args> >(args)...) {}
+
+		lambda_state_t(const lambda_state_t &other)
+			:lambda_state_base_t<Base>(other.params, std::index_sequence_for<Args...>())
+			,params(params) {}
+
+		lambda_state_t(lambda_state_t &&other)
+			:lambda_state_base_t<Base>(other.params, std::index_sequence_for<Args...>())
+			,params(std::move(params)) {}
+
+	protected:
+		std::tuple<Args...> params;
+	};
+
 }
+
+///Helps to define state variable of the shared function for immovable objects
+/**
+ * Immovable objects cannot be used in capture list, because the object of the
+ * lamba function is moved or copied several times to achieve creation of
+ * the shared_function. The lambda_state provides "fake" moving and "fake" copying
+ * constructor. When immovable object is copied or moved, it
+ * is constructed again using initail arguments.
+ *
+ * Note that using lambda_date is slow, because object is create and destroyed
+ * by several time. If the object is movable, you don't need to use
+ * lambda_state.
+ *
+ * Following example creates mutex as internal state of the function. While
+ * the function can be shared, the mutex protects content of the function
+ * while it is called by multiple threads.
+ *
+ *
+ * @code
+ * auto fn = [lock = lambda_state<std::mutex>()]() mutable {
+ *    std::unique_lock<std_mutex> locked(lock);
+ *    //... some content
+ * };
+ * @endcode
+ *
+ * @tparam T object to create as internal lambda's state
+ * @param args optional arguments to construct the object
+ * @return
+ */
+template<typename T, typename ... Args>
+auto lambda_state(Args && ... args) {
+	return _details::lambda_state_t<T, Args ... >(std::forward<Args>(args)...);
+}
+
+///Shared function is function shared to many places, while it maintains its internal state
+/**
+ * Shared functions have purpose to maintain its internal state. Common function
+ * are copied everytime the variable is copied including of their internal state.
+ * This is also case of std::function.
+ *
+ * shared_function is not copied, it is shared, so its internal state is kept
+ * between copies.
+ */
 
 template<typename Proto>
 class shared_function;
+
 
 
 template<typename Ret, typename ... Args>
@@ -65,6 +146,9 @@ public:
 
 	template<typename T>
 	shared_function(T &&fn):fn(new FnWrap<T>(std::forward<T>(fn))) {}
+	shared_function(const shared_function &other):fn(other.fn) {}
+	shared_function(shared_function &other):fn(other.fn) {}
+	shared_function(shared_function &&other):fn(std::move(other.fn)) {}
 	shared_function() {}
 
 	bool operator==(const shared_function &other) const {return fn == other.fn;}
--- test/shared_function.cpp
+++ test/shared_function.cpp
@@ -8,32 +8,46 @@
 #include "../shared_function.h"
 #include <fstream>
 #include <iostream>
+#include <mutex>
+#include <thread>
+#include <chrono>
 
 using namespace ondra_shared;
+using namespace std::chrono_literals;
 
 auto create_reader(const std::string &file) {
-/*	class IOWrap: public std::ifstream {
-	public:
-		IOWrap(const std::string &s):std::ifstream(s),s(s) {}
-		IOWrap(const IOWrap &other):std::ifstream(other.s),s(s) {}
-	}*/
 
 	std::ifstream input(file);
-	return [input = std::move(input)]() mutable {
+	if (!input) throw std::runtime_error("file not found");
+
+	return [input = std::move(input),mx=lambda_state<std::mutex>()]() mutable {
+		std::this_thread::sleep_for(100ms);
+		std::unique_lock<std::mutex> _(mx);
 		return input.get();
 	};
 }
 
 
 int main(int, char **) {
+
 	shared_function<int()> reader(create_reader("shared_function.cpp"));
 
-	int c = reader();
-	while (c != EOF) {
-		std::cout.put(static_cast<char>(c));
-		c = reader();
-	}
+	auto readfn = [=] {
+		int c = reader();
+		while (c != EOF) {
+			std::cout.put(static_cast<char>(c));
+			c = reader();
+		}
+	};
+	std::thread t1(readfn);
+	std::thread t2(readfn);
+	std::thread t3(readfn);
+	std::thread t4(readfn);
 
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
 
 
 	return 0;
