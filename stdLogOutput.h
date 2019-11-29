8ca717d618c8be51460daf1ec9914e27068f4888
Ondrej Novak <ondra-novak@email.cz>
2019-11-29 20:08:07 +0100

    separate parts of the log line to achieve easier customization

--- stdLogOutput.h
+++ stdLogOutput.h
@@ -41,6 +41,7 @@ public:
 
 
 
+
 protected:
 	std::mutex lock;
 	LogLevel enabledLevel;
@@ -60,14 +61,14 @@ public:
 		:ident((other.ident+"][").append(ident.data, ident.length))
 		,shared(other.shared) {}
 
-	virtual bool start(LogLevel level, MutableStrViewA &buffer);
-	virtual void sendBuffer(MutableStrViewA &text);
-	virtual void commit(const MutableStrViewA &text);
-	virtual PLogProvider newSection(const StrViewA &ident) ;
-	virtual void setProgress(float progressVal, int expectedCycles) ;
+	virtual bool start(LogLevel level, MutableStrViewA &buffer) override;
+	virtual void sendBuffer(MutableStrViewA &text) override;
+	virtual void commit(const MutableStrViewA &text) override;
+	virtual PLogProvider newSection(const StrViewA &ident)  override;
+	virtual void setProgress(float progressVal, int expectedCycles)  override;
 
 
-	virtual bool isLogLevelEnabled(LogLevel level) const {
+	virtual bool isLogLevelEnabled(LogLevel level) const  override{
 		return shared->isLogLevelEnabled(level);
 	}
 protected:
@@ -80,8 +81,11 @@ protected:
 
 	void finishBuffer(const MutableStrViewA &b);
 	void prepareBuffer(MutableStrViewA &b);
+	static unsigned int getThreadIdent();
 
-
+	virtual void appendDate(std::time_t now);
+	virtual void appendLevel(LogLevel level);
+	virtual void appendThreadIdent();
 };
 
 inline PLogProvider StdLogProviderFactory::create() {
@@ -96,59 +100,74 @@ inline void StdLogProvider::setProgress(float , int ) {
 	//not implemented
 }
 
+inline unsigned int StdLogProvider::getThreadIdent() {
+	static std::atomic<unsigned int> threadCounter;
+	static thread_local unsigned int curThreadId = ++threadCounter;
+	return curThreadId;
+}
+
+inline void StdLogProvider::appendDate(std::time_t now) {
+
+	auto wrfn = [&](char c) { buffer.push_back(c);};
+
+	std::tm tinfo;
+	gmtime_r(&now, &tinfo);
+
+	unsignedToString(tinfo.tm_year+1900, wrfn, 10,4);
+	buffer.push_back('-');
+	unsignedToString(tinfo.tm_mon+1, wrfn, 10,2);
+	buffer.push_back('-');
+	unsignedToString(tinfo.tm_mday, wrfn, 10,2);
+	buffer.push_back(' ');
+	unsignedToString(tinfo.tm_hour, wrfn, 10,2);
+	buffer.push_back(':');
+	unsignedToString(tinfo.tm_min, wrfn, 10,2);
+	buffer.push_back(':');
+	unsignedToString(tinfo.tm_sec, wrfn, 10,2);
+	buffer.push_back(' ');
+}
+
+inline void StdLogProvider::appendLevel(LogLevel level) {
+	auto wrstr = [&](StrViewA c) { for (char x:c) buffer.push_back(x);};
+
+	switch (level) {
+	case LogLevel::fatal: wrstr("FATAL");break;
+	case LogLevel::error: wrstr("Error");break;
+	case LogLevel::warning: wrstr("Warn.");break;
+	case LogLevel::note: wrstr("Note ");break;
+	case LogLevel::info: wrstr("info ");break;
+	case LogLevel::debug: wrstr("debug");break;
+	default:
+	case LogLevel::progress: wrstr("     ");break;
+	}
+	buffer.push_back(' ');
+
+}
+
+inline void StdLogProvider::appendThreadIdent() {
+	auto curThreadId = getThreadIdent();
+	auto wrstr = [&](StrViewA c) { for (char x:c) buffer.push_back(x);};
+	auto wrfn = [&](char c) { buffer.push_back(c);};
+
+	buffer.push_back('[');
+	unsignedToString(curThreadId, wrfn, 10,4);
+	wrstr(ident);
+	wrstr("] ");
+
+}
 
 inline bool StdLogProvider::start(LogLevel level, MutableStrViewA& b) {
 
 	if (shared->isLogLevelEnabled(level)) {
-
-		static std::atomic<unsigned int> threadCounter;
-		static thread_local unsigned int curThreadId = ++threadCounter;
-
-
 		curLevel = level;
 		buffer.clear();
+		std::time(&lastTime);
 
-		auto wrfn = [&](char c) { buffer.push_back(c);};
-		auto wrstr = [&](StrViewA c) { for (char x:c) buffer.push_back(x);};
-
-		std::time_t now;
-		std::tm tinfo;
-		std::time(&now);
-		gmtime_r(&now, &tinfo);
-
-		unsignedToString(tinfo.tm_year+1900, wrfn, 10,4);
-		buffer.push_back('-');
-		unsignedToString(tinfo.tm_mon+1, wrfn, 10,2);
-		buffer.push_back('-');
-		unsignedToString(tinfo.tm_mday, wrfn, 10,2);
-		buffer.push_back(' ');
-		unsignedToString(tinfo.tm_hour, wrfn, 10,2);
-		buffer.push_back(':');
-		unsignedToString(tinfo.tm_min, wrfn, 10,2);
-		buffer.push_back(':');
-		unsignedToString(tinfo.tm_sec, wrfn, 10,2);
-		wrstr(" ");
-
-		switch (level) {
-		case LogLevel::fatal: wrstr("FATAL");break;
-		case LogLevel::error: wrstr("Error");break;
-		case LogLevel::warning: wrstr("Warn.");break;
-		case LogLevel::note: wrstr("Note ");break;
-		case LogLevel::info: wrstr("info ");break;
-		case LogLevel::debug: wrstr("debug");break;
-		default:
-		case LogLevel::progress: wrstr("     ");break;
-		}
-
-		buffer.push_back(' ');
-		buffer.push_back('[');
-		unsignedToString(curThreadId, wrfn, 10,4);
-		wrstr(ident);
-		wrstr("] ");
-
+		appendDate(lastTime);
+		appendLevel(level);
+		appendThreadIdent();
 		b = MutableStrViewA(buffer.data(), buffer.size());
 		prepareBuffer(b);
-		lastTime = now;
 		return true;
 	} else {
 		return false;
