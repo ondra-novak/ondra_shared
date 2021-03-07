ecb85397a6a464939d9db4b4c90e29b826e450d8
Ondřej Novák <ondra-novak@email.cz>
2021-03-07 01:17:40 +0100

    improve filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -5,9 +5,6 @@
  *      Author: ondra
  */
 
-#ifndef SRC_SHARED_FILESYSTEM_H_
-#define SRC_SHARED_FILESYSTEM_H_
-
 #ifdef __GNUC__
 #if __GNUC__ < 8
 #include <experimental/filesystem>
@@ -21,5 +18,3 @@ namespace std {
 #include <filesystem>
 #endif
 
-
-#endif /* SRC_SHARED_FILESYSTEM_H_ */
